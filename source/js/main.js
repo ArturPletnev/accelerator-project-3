97005a24362fc2d602333e9ad85508e4440b083f
Artur Pletnev <art-pletnev@mail.ru>
2025-02-07 11:26:00 +0300

    4

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,11 @@
 import { toggleBurgerHandler } from './header-toggle';
 import { navSubmenuHandler } from './nav-submenu';
 import { initHeroSlider } from './slider-hero';
+import { initializeModalHandler } from './modal';
+import { initProgramsSlider } from './slider-programs';
 
 toggleBurgerHandler(); // Открытие меню в Header
 navSubmenuHandler(); // Открытие аккордионов в меню навигации
 initHeroSlider(); // Слайдер в блоке Hero
+initializeModalHandler(); // Модальное окно в блоке About
+initProgramsSlider(); // Слайдер в блоке Programs
